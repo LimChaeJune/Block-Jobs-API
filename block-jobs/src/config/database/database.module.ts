57676d1f084cfffdb7244a50aae6d90daf9749a5
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-06-19 02:03:29 +0900

    Update database.module.ts

--- block-jobs/src/config/database/database.module.ts
+++ block-jobs/src/config/database/database.module.ts
@@ -2,18 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import entities from 'src/typeorm/index.entity';
-import configuration from '../configuration';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
         type: 'mysql',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: config.get<string>('db.host'),
+        port: config.get<number>('db.port'),
+        username: config.get<string>('db.user'),
+        password: config.get<string>('db.password'),
+        database: config.get<string>('db.name'),
         entities: entities,
         synchronize: false,
       }),
