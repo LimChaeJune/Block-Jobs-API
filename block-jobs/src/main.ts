d18e6b0bc87def7d856c2a6b9e1b1f8c00fd6022
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-06-19 02:37:07 +0900

    upt

--- block-jobs/src/main.ts
+++ block-jobs/src/main.ts
@@ -1,14 +1,25 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/setupSwagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug'],
-  });
+  try {
+    const app = await NestFactory.create(AppModule, {
+      logger: ['error', 'warn', 'log', 'debug'],
+    });
 
-  setupSwagger(app);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+    setupSwagger(app);
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
+    Logger.log(process.env.PORT);
+    Logger.log(process.env.DB_HOST);
+    Logger.log(process.env.DB_PORT);
+    Logger.log(process.env.DB_USER);
+    Logger.log(process.env.DB_PASSWORD);
+    Logger.log(process.env.DB_NAME);
+  } catch (error) {
+    Logger.log(error);
+  }
 }
 bootstrap();
